3f35357a078904a71182ae5aeb1e5692b93dac01
Jonathan Enrique Tejeda <askjonathantejeda@gmail.com>
2026-04-28 20:37:30 -0400

    Add Selenium test

---
+++ test_selenium_google.py
@@ -0,0 +1,18 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def test_google_title():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+
+    try:
+        driver.get("https://www.google.com")
+        assert "Google" in driver.title
+    finally:
+        driver.quit()
